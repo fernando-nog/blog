ee4ad389abfc940b824d10e6f11feb1e0fddc7cf
Fernando Nogueira <fernando.a.gomes.n@gmail.com>
2025-08-28 13:01:39 +0200

    Enhance SEO by adding meta tags for site verification

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,19 @@ import Seo from "../components/seo"
 const HomePage = () => {
   return (
     <>
-      <Seo title="Fernando Nogueira - Tech Lead & Software Engineer" />
+      <Seo 
+        title="Fernando Nogueira - Tech Lead & Software Engineer" 
+        meta={[
+          {
+            name: "msvalidate.01",
+            content: "527715A695666B798E1D18C3FD66E418"
+          },
+          {
+            name: "google-site-verification",
+            content: "ALlOLnq78qnmghuLq694BDWmPNqOfQTTTvf42ufWNNw"
+          }
+        ]}
+      />
       <LandingPage />
     </>
   )
